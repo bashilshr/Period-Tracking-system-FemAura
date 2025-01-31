582100c37d1fbe13b7cc7e1be9923caba51911ce
bashilshr <bashil.shrestha@gmail.com>
2025-01-31 13:02:26 +0545

    pre-commit setting up porject using clean architecture

--- FemAura/UserAuth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('otp-verify/', views.otp_verify_view, name='otp_verify'),
-]
